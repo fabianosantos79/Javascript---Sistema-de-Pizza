cf1e252a134a725caecdf8c82f51c1728b56a973
Fabiano <fabiano.ssantos79@gmail.com>
2022-06-05 18:43:51 -0300

    upload index

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let modalQt;
+
+//Listagem das pizzas
 pizzaJson.map((item, index) => {
     //Clonando o modelo com a quantidade do pizzas.js
     let pizza = document.querySelector('.models .pizza-item').cloneNode(true);
@@ -15,16 +18,26 @@ pizzaJson.map((item, index) => {
 
         //variavel para armazenar a consulta da API
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
 
+        //Preenchendo os dados no Modal
         document.querySelector('.pizzaBig img').src = pizzaJson[key].img;
         document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         document.querySelector('.pizzaInfo .pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
-        document.querySelector('.pizzaInfo--size.selected').classList.remove('selected');
+        
+        //Percorrendo os tamanhos das pizzas
         document.querySelectorAll('.pizzaInfo--size').forEach((size, sizeIndex) =>{
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
         })
+        
+        //Preenchendo a quantidade das pizzas
+        document.querySelector('.pizzaInfo--qt').innerHTML = modalQt;
 
+        //Colocando a div pai no body
         document.querySelector('.pizzaWindowArea').style.display = 'flex';
         
     })
@@ -35,7 +48,39 @@ pizzaJson.map((item, index) => {
         document.querySelector('.pizzaWindowArea').style.display = 'none';
     })
 
+    document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('.pizzaWindowArea').style.display = 'none';
+    })
+
     //Inserir os clones no body da página - Cards de pizza
     document.querySelector('.pizza-area').append(pizza);
 
 });
+
+//>>>>>> EVENTOS DO MODAL <<<<<<<
+
+//Botão menos
+document.querySelector('.pizzaInfo--qtmenos').addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    if(modalQt > 1){
+        modalQt--;
+        document.querySelector('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+
+//Botão Mais
+document.querySelector('.pizzaInfo--qtmais').addEventListener('click', e =>{
+    e.preventDefault();
+    modalQt++;
+    document.querySelector('.pizzaInfo--qt').innerHTML = modalQt;
+})
+
+//Selecionar tamanhos
+document.querySelectorAll('.pizzaInfo--size').forEach((size, sizeIndex) =>{
+    size.addEventListener('click', (e) =>{
+        document.querySelector('.pizzaInfo--size.selected').classList.remove('selected');    
+        size.classList.add('selected');
+    })
+})
